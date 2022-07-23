1cf0c41f16068d6c4b0e7056dd5bca00ab40bd80
gary <ceouls@gmail.com>
2022-07-22 17:15:05 -0700

    init

---
+++ index.js
@@ -0,0 +1,366 @@
+// const audio = new Audio("C.wav");
+
+// document.onclick = function () {
+//     audio.play();
+// }
+
+
+function playclip() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineC.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[0];
+            audio.play();
+        }
+    }
+}
+
+function playclip2() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineCsharp.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[1];
+            audio.play();
+        }
+    }
+}
+
+function playclip3() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineD.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[2];
+            audio.play();
+        }
+    }
+}
+
+function playclip4() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineDsharp.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[3];
+            audio.play();
+        }
+    }
+}
+
+function playclip5() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineE.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[4];
+            audio.play();
+        }
+    }
+}
+
+function playclip6() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineF.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[5];
+            audio.play();
+        }
+    }
+}
+
+function playclip7() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineFsharp.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[6];
+            audio.play();
+        }
+    }
+}
+
+function playclip8() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineG.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[7];
+            audio.play();
+        }
+    }
+}
+
+function playclip9() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineGsharp.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[8];
+            audio.play();
+        }
+    }
+}
+
+function playclip10() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineA.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[9];
+            audio.play();
+        }
+    }
+}
+
+function playclip11() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineAsharp.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[10];
+            audio.play();
+        }
+    }
+}
+
+function playclip12() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineB.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[11];
+            audio.play();
+        }
+    }
+}
+
+function playclip13() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineC-2.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[12];
+            audio.play();
+        }
+    }
+}
+
+function playclip14() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineCsharp-2.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[13];
+            audio.play();
+        }
+    }
+}
+
+function playclip15() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineD-2.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[14];
+            audio.play();
+        }
+    }
+}
+
+function playclip16() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineDsharp-2.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[15];
+            audio.play();
+        }
+    }
+}
+
+function playclip17() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineE-2.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[16];
+            audio.play();
+        }
+    }
+}
+
+function playclip18() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineF-2.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[17];
+            audio.play();
+        }
+    }
+}
+
+function playclip19() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineFsharp-2.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[18];
+            audio.play();
+        }
+    }
+}
+
+function playclip20() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineG-2.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[19];
+            audio.play();
+        }
+    }
+}
+
+function playclip21() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineGsharp-2.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[20];
+            audio.play();
+        }
+    }
+}
+
+function playclip22() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineA-2.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[21];
+            audio.play();
+        }
+    }
+}
+
+function playclip23() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineAsharp-2.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[22];
+            audio.play();
+        }
+    }
+}
+
+function playclip24() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7") != -1) || (navigator.appVersion.indexOf("MSIE 8") != -1)) {
+        if (document.all) {
+            document.all.sound.src = "/sine/sineF-2.mp3";
+        }
+    }
+
+    else {
+        {
+            let audio = document.getElementsByTagName("audio")[23];
+            audio.play();
+        }
+    }
+}
